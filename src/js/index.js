5bb26ac7d0b103f1f06e1df2ab578778d0113ec0
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-01 23:25:59 +0900

    Feat : 메뉴 추가 - 사용자 입력값을 메뉴 리스트에 추가
    querySelector 코드 간소화
    
    추가되는 메뉴의 마크업 템플릿 변수 선언
    메뉴 템플릿에 입력받은 메뉴 이름 가져오기
    
    Enter key를 입력받으면 ul 태그 안에 마크업 언어 작성
    
    insertAdjacentHTML을 통해 추가될 위치 조정

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,34 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
     // form 태그가 자동으로 전송되는걸 막기(제출시 새로고침 되는 현상)
-    document.querySelector('#espresso-menu-form').addEventListener('submit', (e) => {
+    $('#espresso-menu-form').addEventListener('submit', (e) => {
         e.preventDefault();
     })
     // 메뉴의 이름을 입력 받기
-    document.querySelector('#espresso-menu-name').addEventListener('keypress', (e) => {
+    $('#espresso-menu-name').addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
-            console.log(document.querySelector('#espresso-menu-name').value);
+            const espressoMenuName = $('#espresso-menu-name').value;
+            const menuItemTemplate = (espressoMenuName) => {
+                return `
+                <li class="menu-list-item d-flex items-center py-2">
+                    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                    <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                    >
+                        수정
+                    </button>
+                    <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                    >
+                        삭제
+                    </button>
+                </li>`;
+            };
+            $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
+
         }
     });
 }
