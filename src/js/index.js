a826f09cea5fa54da4bae8797a6c5f3f3acae44e
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-03 20:16:43 +0900

    Refactor : 메뉴 추가하기
    html tag 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import { menuMarkUpMessage } from "./messages.js";
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
@@ -9,22 +10,7 @@ function App() {
 
         const espressoMenuName = $('#espresso-menu-name').value;
         const menuItemTemplate = (espressoMenuName) => {
-            return `
-                <li class="menu-list-item d-flex items-center py-2">
-                    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-                    <button
-                        type="button"
-                        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                    >
-                        수정
-                    </button>
-                    <button
-                        type="button"
-                        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                    >
-                        삭제
-                    </button>
-                </li>`;
+            return menuMarkUpMessage(espressoMenuName)
         };
         $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
         updateMenuCounts();
