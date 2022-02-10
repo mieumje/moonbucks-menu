a0fa255b0f3102b80444a062c9ff72f3800c71b6
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-10 21:49:15 +0900

    Refactor : API 호출 객체 추상화
    request 객체로 fetch를 관리
    서버에 어떤 요청을 한 뒤 응답이 데이터를 같이 주는 것과 응답만 보내는 경우가 있어 두 개의 request 객체로 관리
    
    HTTP_METHOD 객체로 fetch의 두 번째 요소인 option을 관리

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,38 +1,59 @@
 const BASE_URL = 'http://localhost:3000';
 
-const MenuApi = {
-    async getAllMenuByCategory(category) {
-        const response = await fetch(`${BASE_URL}/api/category/${category}/menu`);
-        return response.json();
-    },
-    async createMenu(category, name) {
-        const response = await fetch(`${BASE_URL}/api/category/${category}/menu`, {
+const HTTP_METHOD = {
+    POST(data) {
+        return {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name }),
-        });
-        if (!response.ok) console.log("에러가 발생했습니다.");
+            body: JSON.stringify(data),
+        }
     },
-    async updateMenu(category, name, menuId) {
-        const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}`, {
+    PUT(data) {
+        return {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name }),
-        });
-        if (!response.ok) console.log("에러가 발생했습니다.");
-        return response.json()
+            body: data ? JSON.stringify(data) : null,
+        }
+    },
+    DELETE() {
+        return {
+            method: "DELETE",
+        }
+    },
+};
+
+const request = async (url, option) => {
+    const response = await fetch(url, option);
+    if (!response.ok) {
+        alert('에러가 발생했습니다.');
+        console.error(e);
+    }
+    return response.json();
+};
+const requestWithoutJson = async (url, option) => {
+    const response = await fetch(url, option);
+    if (!response.ok) {
+        alert('에러가 발생했습니다.');
+        console.error(e);
+    }
+    return response
+}
+
+const MenuApi = {
+    getAllMenuByCategory(category) {
+        return request(`${BASE_URL}/api/category/${category}/menu`)
+    },
+    createMenu(category, name) {
+        return request(`${BASE_URL}/api/category/${category}/menu`, HTTP_METHOD.POST({ name }))
+    },
+    updateMenu(category, name, menuId) {
+        return request(`${BASE_URL}/api/category/${category}/menu/${menuId}`, HTTP_METHOD.PUT({ name }))
     },
     async toggleSoldOutMenu(category, menuId) {
-        const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}/soldout`, {
-            method: "PUT",
-        });
-        if (!response.ok) console.log("에러가 발생했습니다.");
+        return request(`${BASE_URL}/api/category/${category}/menu/${menuId}/soldout`, HTTP_METHOD.PUT({ menuId }))
     },
     async deleteMenu(category, menuId) {
-        const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}`, {
-            method: "DELETE",
-        });
-        if (!response.ok) console.log("에러가 발생했습니다.");
+        return requestWithoutJson(`${BASE_URL}/api/category/${category}/menu/${menuId}`, HTTP_METHOD.DELETE())
     },
 }
 export default MenuApi;
